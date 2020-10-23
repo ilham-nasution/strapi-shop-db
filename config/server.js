d8815dd7202412bf917d88b734419b515be32d47
Ilham Rahman <ilhaamrahman@gmail.com>
2020-10-23 13:50:50 +0700

    Initialize strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e978a299cd4efdc9dfd58bdea18d389c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2988263a-f9b2-48a5-80c9-de587f012c76'
+};
